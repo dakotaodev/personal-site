723b146a16ab844e68999a16853148a4a042fd98
Dakota <dakota.o.dev@gmail.com>
2025-12-29 20:47:57 -0800

    Add new blog post for interesting links and update asset imports

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_ZulDpU from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
-export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU]]);
+import __ASTRO_IMAGE_IMPORT_12Rx35 from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post+copy.md";
+import __ASTRO_IMAGE_IMPORT_26vCsv from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Finteresting-links.md";
+export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU], ["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post+copy.md", __ASTRO_IMAGE_IMPORT_12Rx35], ["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Finteresting-links.md", __ASTRO_IMAGE_IMPORT_26vCsv]]);
 		
---
+++ src/content/blog/interesting-links.md
@@ -0,0 +1,8 @@
+---
+title: 'Interesting Links'
+description: 'Updated upon discovery!'
+pubDate: 'Dec 29 25'
+---
+
+### Blogs and Articles
+- ["The Grug Brained Developer"](https://grugbrain.dev/)
