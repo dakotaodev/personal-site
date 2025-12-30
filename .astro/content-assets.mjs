574b06c206febe9304a47a469bf9dbd3e92048f6
Dakota <dakota.o.dev@gmail.com>
2025-12-29 19:17:40 -0800

    Add blog content schema, data store, and initial blog post assets

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_ZulDpU from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
+export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU]]);
+		
