e58e5481ac485e685618aea27772880efcbeb640
Dakota <dakota.o.dev@gmail.com>
2026-02-23 20:33:52 -0800

    Refactor global styles in CSS: update design tokens, reset styles, and improve typography for better readability and consistency across the site.

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,4 @@
 
 import __ASTRO_IMAGE_IMPORT_ZulDpU from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md";
-import __ASTRO_IMAGE_IMPORT_12Rx35 from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post+copy.md";
-import __ASTRO_IMAGE_IMPORT_26vCsv from "../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Finteresting-links.md";
-export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU], ["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post+copy.md", __ASTRO_IMAGE_IMPORT_12Rx35], ["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Finteresting-links.md", __ASTRO_IMAGE_IMPORT_26vCsv]]);
+export default new Map([["../../assets/blog-placeholder-3.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Ffirst-post.md", __ASTRO_IMAGE_IMPORT_ZulDpU]]);
 		
